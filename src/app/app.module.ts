7aa89346edc04d867f7bedb1f83c33726cdb176f
Daniela <goulartsdaniela@gmail.com>
2022-03-28 15:21:49 -0200

    Correção de bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './componentes/menu/menu.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
-import { LoginComponent } from './componentes/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { FeedComponent } from './pages/feed/feed.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
