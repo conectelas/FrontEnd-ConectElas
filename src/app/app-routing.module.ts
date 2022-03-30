8453759a505a57fec176e1d7132b1c7fad35a7ec
Daniela <goulartsdaniela@gmail.com>
2022-03-30 18:14:07 -0300

    estilização

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
 
-  {path: '', redirectTo: 'entrar',pathMatch: 'full'},
+  {path: '', redirectTo: 'login',pathMatch: 'full'},
 
   {path: 'login', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent}
