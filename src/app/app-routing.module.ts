b27e839fa31a15c553b184299fc38d044c94aa2d
Vanessa Navarro <98328341+vanessa-nvr@users.noreply.github.com>
2022-04-04 05:48:28 -0300

    Criação do sobre nós e o projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [
+
+  {path: '', redirectTo: 'login',pathMatch: 'full'},
+
+  {path: 'login', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    LoginComponent,
+    CadastroComponent,
+    HomeComponent,
+    PerfilComponent,
+    FeedComponent,
+    SobreComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
