346f4845540bda9b8cc09d03ae6f25bd7f3ae4f2
Leonardo <leo@pop-os.localdomain>
2022-04-11 18:30:06 -0300

    token para criacao de ADM feito

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment = {
   tipo: '',
   foto: '',
   totalPosts: 0,
+  tokenADM: 'vitorlindao',
   server: 'http://localhost:8080/',
   baseUrl: 'https://conectelas.netlify.app',
 };
